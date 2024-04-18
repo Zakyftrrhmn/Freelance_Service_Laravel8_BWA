79f3d88366bc66ae45352e86a3d22d319d21ebd8
unknown <zakifaturrahman0@gmail.com>
2024-04-18 22:17:18 +0700

    Handling Assets, Css & js

---
+++ public/js/toggleModal.js
@@ -0,0 +1,6 @@
+function toggleModal(modalID){
+    document.getElementById(modalID).classList.toggle("hidden");
+    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+    document.getElementById(modalID).classList.toggle("flex");
+    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+}
